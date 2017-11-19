06697a3967f58aafbedd28e3b5e6f6d1a36eaae7
Aamir Sheikh <sheikhaamir.01@gmail.com>
2017-11-20 03:22:07 +0530

    Updating the code

---
+++ angularjs-spring/src/main/java/com/jeejava/dto/Product.java
@@ -0,0 +1,114 @@
+package com.jeejava.dto;
+
+public class Product {
+
+	private int id;
+	private String name;
+	private double price;
+	private String bugId;
+	private String location;
+	private String status;
+	private String type;
+
+	public Product() {
+	}
+
+	public Product(int id, String name, double price) {
+		this.id = id;
+		this.name = name;
+		this.price = price;
+	}
+	
+
+	public Product(int id, String name, double price, String location, String status, String type) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.bugId = bugId;
+		this.location = location;
+		this.status = status;
+		this.type = type;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	public String getBugId() {
+		return bugId;
+	}
+
+	public void setBugId(String bugId) {
+		this.bugId = bugId;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	
+	
+}
